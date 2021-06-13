c1e0568edaa6630272490dc4840eefb288d2cde0
Steven Ennis <steven.otieno1@gmail.com>
2021-06-13 21:23:09 +0300

    feature(header/footer): implement header/footer
    - add header and footer component
    
    [starts #008]

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+export const Footer = () => {
+  return <footer>Fruits &amp; Veggies  &copy; 2021</footer>;
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/Header.module.css";
+
+export const Header = () => {
+  const router = useRouter();
+  const isHome = router.pathname === "/";
+
+  const goBack = (event) => {
+    event.preventDefault();
+    router.back();
+  };
+  return (
+    <div className={styles.nav}>
+      {!isHome && (
+        <div className={styles.back}>
+          <a href="#" onClick={goBack}>
+            &lt; Back
+          </a>
+        </div>
+      )}
+      <div className={styles.title}>
+        <Link href="/">
+          <a>
+            <h1>Fruits &amp; Veggies</h1>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Header;
