02671aca762a250dc3bb0f1ee3142e113e21b6ad
Steven Ennis <steven.otieno1@gmail.com>
2021-06-14 21:14:08 +0300

    feature(auth): implement login & logout method
    - add login and logout method
    
    [Starts #003]

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -6,6 +6,22 @@ const AuthContext = createContext();
 export const AuthProvider = (props) => {
   const [user, setUser] = useState(null);
 
+  /**
+   * Adds email to user
+   * @param {string} email 
+   */
+  const loginUser = async (email) => {
+    setUser({ email });
+  };
+
+  /**
+   *  Sets email to null and logs out the user.
+   * @param {*} email 
+   */
+  const logoutUser = async (email) => {
+    setUser(null);
+  };
+
   return <AuthProvider>{props.children}</AuthProvider>;
 };
 
